31a1befaed14caeba03e199ac880a142c419a455
Lee Robert <lrobert@leerobert.ca>
2023-12-02 14:45:29 -0500

    feat: Add solution to day one
    Was harder than expected, edge cases such as eightwo really threw me off on what was supposed to be valid

--- internal/day_1/run.go
+++ internal/day_1/run.go
@@ -5,8 +5,13 @@ import (
 )
 
 func Run() {
-	util.AssertResult("Part 1 example.txt", runPartOne("./inputs/1/example.txt"), "0")
-	util.AssertResult("Part 1 intput.txt", runPartOne("./inputs/1/input.txt"), "0")
-	util.AssertResult("Part 2 example.txt", runPartTwo("./inputs/1/example.txt"), "0")
-	util.AssertResult("Part 2 input.txt", runPartTwo("./inputs/1/input.txt"), "0")
+	util.AssertResult("Part 1 example.txt", runPartOne("./internal/day_1/example.txt"), "142")
+	util.AssertResult("Part 1 input.txt", runPartOne("./internal/day_1/input.txt"), "54953")
+
+	util.AssertResult("Part 2 example2.txt", runPartTwo("./internal/day_1/example2.txt"), "281")
+
+	// 51175 is too low
+	// 53868 is just right
+	// 53885 is too high
+	util.AssertResult("Part 2 input.txt", runPartTwo("./internal/day_1/input.txt"), "53868")
 }
