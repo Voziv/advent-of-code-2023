15da589105b272243d2ce8c41b8744ea58d3a409
Lee Robert <lrobert@leerobert.ca>
2023-12-02 14:49:30 -0500

    feat: Add skeleton for day 2

--- cmd/aoc/aoc.go
+++ cmd/aoc/aoc.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"github.com/voziv/advent-of-code-2023/internal/day_1"
+	"github.com/voziv/advent-of-code-2023/internal/day_2"
 	"os"
 )
 
 var DAYS = map[string]func(){
 	"1": day_1.Run,
+	"2": day_2.Run,
 }
 
 func main() {
---
+++ internal/day_2/run.go
@@ -0,0 +1,11 @@
+package day_2
+
+import (
+	"github.com/voziv/advent-of-code-2023/internal/util"
+)
+
+func Run() {
+	util.AssertResult("Part 1 example.txt", runPartOne("./internal/day_2/example.txt"), "8")
+	//util.AssertResult("Part 1 input.txt", runPartOne("./internal/day_2/input.txt"), "")
+
+}
