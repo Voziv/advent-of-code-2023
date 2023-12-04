c24fa2c489ce6290cb90319f83658ace511042a5
Lee Robert <lrobert@leerobert.ca>
2023-12-04 15:35:45 -0500

    feat: Add solution for day 2 part 2

--- internal/day_2/run.go
+++ internal/day_2/run.go
@@ -8,4 +8,7 @@ func Run() {
 	util.AssertResult("Part 1 example.txt", runPartOne("./internal/day_2/example.txt"), "8")
 	util.AssertResult("Part 1 input.txt", runPartOne("./internal/day_2/input.txt"), "2149")
 
+	util.AssertResult("Part 2 example.txt", runPartTwo("./internal/day_2/example.txt"), "2286")
+	util.AssertResult("Part 2 input.txt", runPartTwo("./internal/day_2/input.txt"), "2286")
+
 }
