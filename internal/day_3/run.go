4ebd8abd4562a08b9d869a88ee003403c0ae2bb0
Lee Robert <lrobert@leerobert.ca>
2023-12-05 13:20:26 -0500

    feat(day_3): add solution for part 2
    Really not proud of this one, it's pretty ugly code

--- internal/day_3/run.go
+++ internal/day_3/run.go
@@ -8,7 +8,6 @@ func Run() {
 	util.AssertResult("Part 1 example.txt", runPartOne("./internal/day_3/example.txt"), "4361")
 	util.AssertResult("Part 1 input.txt", runPartOne("./internal/day_3/input.txt"), "540025")
 
-	util.AssertResult("Part 2 example.txt", runPartTwo("./internal/day_2/example.txt"), "467835")
-	util.AssertResult("Part 2 input.txt", runPartTwo("./internal/day_2/input.txt"), "")
-
+	util.AssertResult("Part 2 example.txt", runPartTwo("./internal/day_3/example.txt"), "467835")
+	util.AssertResult("Part 2 input.txt", runPartTwo("./internal/day_3/input.txt"), "84584891")
 }
