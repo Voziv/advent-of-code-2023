33ae0e816d0a5b0ee66f6f0b55dc5228c4ab312f
Lee Robert <lrobert@leerobert.ca>
2023-12-18 01:45:55 -0500

    feat(day_0): add empty day 0

---
+++ internal/day_0/run_part_one.go
@@ -0,0 +1,10 @@
+package day_0
+
+import (
+	"github.com/voziv/advent-of-code-2023/internal/util"
+)
+
+func runPartOne(inputFileName string) string {
+	lines := util.GetFileContents(inputFileName)
+	return lines[0]
+}
