2880917032feb44c91f077e3fe588991e3ea96fe
Lee Robert <lrobert@leerobert.ca>
2023-12-18 16:51:59 -0500

    refactor(day4): combine into a single file and use new format

--- internal/day4/run.go
+++ internal/day4/run.go
@@ -2,12 +2,84 @@ package day4
 
 import (
 	"github.com/voziv/advent-of-code-2023/internal/util"
+	"strconv"
+	"strings"
 )
 
-func Run() {
-	util.AssertResult("Part 1 example.txt", runPartOne("./internal/day4/example.txt"), "13")
-	util.AssertResult("Part 1 input.txt", runPartOne("./internal/day4/input.txt"), "18653")
+type result struct {
+	partOne int
+	partTwo int
+}
 
-	util.AssertResult("Part 2 example.txt", runPartTwo("./internal/day4/example.txt"), "30")
-	util.AssertResult("Part 2 input.txt", runPartTwo("./internal/day4/input.txt"), "5921508")
+func Run() {
+	util.AssertResult("example.txt", run("./internal/day4/example.txt"), result{
+		partOne: 13,
+		partTwo: 30,
+	})
+
+	util.AssertResult("input.txt", run("./internal/day4/input.txt"), result{
+		partOne: 18653,
+		partTwo: 5921508,
+	})
+}
+
+func run(inputFileName string) result {
+	lines := util.GetFileContents(inputFileName)
+
+	result := result{
+		partOne: 0,
+		partTwo: 0,
+	}
+	cards := map[int]*Card{}
+	var cardStack []*Card
+	for _, line := range lines {
+		card := parseLineIntoCard(line)
+		cards[card.Id()] = card
+		cardStack = append(cardStack, card)
+
+		matches := card.Matches()
+
+		total := 0
+		if matches > 0 {
+			total = 1
+			for i := 0; i < matches-1; i++ {
+				total = total * 2
+			}
+
+		}
+
+		result.partOne += total
+	}
+
+	// Part Two
+	for i := 0; i < len(cardStack); i++ {
+		matches := cardStack[i].Matches()
+		for j := 1; j <= matches; j++ {
+			card, ok := cards[cardStack[i].Id()+j]
+			if !ok {
+				break
+			}
+			cardStack = append(cardStack, card)
+		}
+	}
+
+	result.partTwo = len(cardStack)
+
+	return result
+}
+
+func parseIntoNumbers(input string) []int {
+	var numbers []int
+
+	numberStrings := strings.Split(input, " ")
+	for _, item := range numberStrings {
+		number, err := strconv.Atoi(item)
+		if err != nil {
+			continue
+		}
+
+		numbers = append(numbers, number)
+	}
+
+	return numbers
 }
