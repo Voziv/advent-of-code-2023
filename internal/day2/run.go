9848bdfb2e7d9fa0cc9730bc95cdd630b74c899b
Lee Robert <lrobert@leerobert.ca>
2023-12-18 16:45:16 -0500

    refactor(day2): combine into a single file and use new format

--- internal/day2/run.go
+++ internal/day2/run.go
@@ -2,13 +2,134 @@ package day2
 
 import (
 	"github.com/voziv/advent-of-code-2023/internal/util"
+	"strconv"
+	"strings"
 )
 
+type day2Answer struct {
+	partOne int
+	partTwo int
+}
+
 func Run() {
-	util.AssertResult("Part 1 example.txt", runPartOne("./internal/day2/example.txt"), "8")
-	util.AssertResult("Part 1 input.txt", runPartOne("./internal/day2/input.txt"), "2149")
+	util.AssertResult("example.txt", run("./internal/day2/example.txt"), day2Answer{
+		partOne: 8,
+		partTwo: 2286,
+	})
 
-	util.AssertResult("Part 2 example.txt", runPartTwo("./internal/day2/example.txt"), "2286")
-	util.AssertResult("Part 2 input.txt", runPartTwo("./internal/day2/input.txt"), "2286")
+	util.AssertResult("input.txt", run("./internal/day2/input.txt"), day2Answer{
+		partOne: 2149,
+		partTwo: 71274,
+	})
+}
 
+type Game struct {
+	Id     int
+	Rounds []Round
+}
+
+type Round struct {
+	Cubes map[string]int
+}
+
+var bagOfCubes = map[string]int{
+	"blue":  14,
+	"green": 13,
+	"red":   12,
+}
+
+func run(inputFileName string) day2Answer {
+
+	lines := util.GetFileContents(inputFileName)
+	answer := day2Answer{
+		partOne: 0,
+		partTwo: 0,
+	}
+
+	for _, line := range lines {
+		game := parseGameFromString(line)
+
+		isValidGame := validateGame(game)
+		if isValidGame {
+			answer.partOne += game.Id
+		}
+
+		answer.partTwo += getPowerOfGame(game)
+	}
+
+	return answer
+}
+
+func validateGame(game *Game) bool {
+	isValid := true
+
+	for _, round := range game.Rounds {
+		for color, count := range round.Cubes {
+			if count > bagOfCubes[color] {
+				isValid = false
+				break
+			}
+		}
+	}
+
+	return isValid
+}
+
+func parseGameFromString(input string) *Game {
+	gamePart, afterGamePart, found := strings.Cut(input, ": ")
+	if !found {
+		panic("Ahhhh no : found on line of input")
+	}
+	_, gameNumberString, _ := strings.Cut(gamePart, " ")
+
+	gameNumber, err := strconv.Atoi(gameNumberString)
+	if err != nil {
+		panic(err)
+	}
+
+	roundStrings := strings.Split(afterGamePart, "; ")
+
+	var rounds []Round
+
+	for _, roundString := range roundStrings {
+		cubes := map[string]int{
+			"blue":  0,
+			"green": 0,
+			"red":   0,
+		}
+
+		cubeStrings := strings.Split(roundString, ", ")
+		for _, cubeString := range cubeStrings {
+			numberString, color, _ := strings.Cut(cubeString, " ")
+			numberOfCubes, _ := strconv.Atoi(numberString)
+			cubes[color] = numberOfCubes
+		}
+
+		rounds = append(rounds, Round{
+			Cubes: cubes,
+		})
+	}
+
+	return &Game{
+		Id:     gameNumber,
+		Rounds: rounds,
+	}
+}
+
+func getPowerOfGame(game *Game) int {
+	minimumNumberOfCubes := map[string]int{
+		"blue":  0,
+		"green": 0,
+		"red":   0,
+	}
+
+	for _, round := range game.Rounds {
+		for color, count := range round.Cubes {
+			if count > minimumNumberOfCubes[color] {
+				minimumNumberOfCubes[color] = count
+			}
+		}
+	}
+
+	return minimumNumberOfCubes["blue"] * minimumNumberOfCubes["green"] * minimumNumberOfCubes["red"]
 }
