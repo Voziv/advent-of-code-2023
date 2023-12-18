3fa0046705ec343e9980a1b50bfd2bf5461c6518
Lee Robert <lrobert@leerobert.ca>
2023-12-18 16:47:37 -0500

    refactor(day0): change result struct name

--- internal/day0/run.go
+++ internal/day0/run.go
@@ -4,26 +4,28 @@ import (
 	"github.com/voziv/advent-of-code-2023/internal/util"
 )
 
-type day0Answer struct {
+type result struct {
 	partOne int
 	partTwo int
 }
 
 func Run() {
-	util.AssertResult("example.txt", run("./internal/day0/example.txt"), day0Answer{
+	util.AssertResult("example.txt", run("./internal/day0/example.txt"), result{
 		partOne: 0,
 		partTwo: 0,
 	})
 
-	util.AssertResult("input.txt", run("./internal/day0/input.txt"), day0Answer{
+	util.AssertResult("input.txt", run("./internal/day0/input.txt"), result{
 		partOne: 0,
 		partTwo: 0,
 	})
 }
 
-func run(inputFileName string) day0Answer {
-	return day0Answer{
+func run(inputFileName string) result {
+	result := result{
 		partOne: 0,
 		partTwo: 0,
 	}
+
+	return result
 }
