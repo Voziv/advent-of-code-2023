3af864881c56739c8354354c9288ea47431c3182
Lee Robert <lrobert@leerobert.ca>
2023-12-18 15:43:17 -0500

    feat(util): add generic support for anything comparable
    This way we don't have to cast ints to strings just for the sake of testing
    
    Also means we can start using structs for answers

--- internal/util/assert_result.go
+++ internal/util/assert_result.go
@@ -5,12 +5,12 @@ import (
 	"os"
 )
 
-func AssertResult(name string, result string, expected string) {
+func AssertResult[R comparable](name string, result R, expected R) {
 	if result != expected {
-		fmt.Printf("[%-20s] Expected %s but got %s instead.\n", name, expected, result)
+		fmt.Printf("[%-15s] Expected %+v but got %+v instead.\n", name, expected, result)
 		fmt.Println("Test failed! Exiting...")
 		os.Exit(1)
 	} else {
-		fmt.Printf("[%-20s] Result: %s\n", name, result)
+		fmt.Printf("[%-15s] Result: %+v\n", name, result)
 	}
 }
